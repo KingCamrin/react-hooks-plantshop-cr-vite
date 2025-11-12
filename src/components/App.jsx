e342ab9c132f41f7a1a4344ed57ad355e9953cbb
Camrin <camrindavis58@gmail.com>
2025-11-12 12:14:07 -0600

    Updated the App.jsx to fetch and a testing array to test the fetch to make sure it works.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
+import PlantList from "./PlantList";
 
-function App() {
+
+
+function App(){
+
+const [plants, setPlants] = useState([]); 
+useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then((r) => r.json())
+  .then((data) => setPlants(data));
+
+  }, []);
   return (
     <div className="app">
       <Header />
       <PlantPage />
+      {/* <PlantList plants={filteredPlants} /> */}
+      <PlantList plants={plants} />
     </div>
   );
 }
 
 export default App;
+
+
--- src/components/NewPlantForm.jsx
+++ src/components/NewPlantForm.jsx
@@ -15,3 +15,4 @@ function NewPlantForm() {
 }
 
 export default NewPlantForm;
+
